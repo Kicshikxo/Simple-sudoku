bcfb4cc7c1430dfc41341d82d6a3f42eceb15add
Kicshikxo <supermegavalera9000@gmail.com>
2023-07-11 06:46:02 +0300

    Create app-container component

---
+++ components/app/container.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="app-container">
+        <slot />
+    </div>
+</template>
+
+<style>
+.app-container {
+    width: 100%;
+    height: 100%;
+    padding: 12px;
+    margin-right: auto;
+    margin-left: auto;
+}
+@media (min-width: 960px) {
+    .app-container {
+        max-width: 900px;
+    }
+}
+@media (min-width: 1264px) {
+    .app-container {
+        max-width: 1185px;
+    }
+}
+@media (min-width: 1904px) {
+    .app-container {
+        max-width: 1785px;
+    }
+}
+</style>
